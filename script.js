6cf5262ac5519bf8ecf0d7e5fc830870b54d3845
Emily Aspiazu <emilyapiazupotes2006@hotmail.com>
2025-06-19 10:58:12 -0500

    Defini la funcion principal para el juego

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+
+// Esta función va a ser como el corazon del juego y se va a repetir muchisimas veces por segundo
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(gameLoop);
+}
